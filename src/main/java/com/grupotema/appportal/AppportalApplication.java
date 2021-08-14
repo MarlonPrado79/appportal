55f06d43a8ef71962d2610f6d1e7a6486a346d23
Marlon Prado <marlonprado.cn@gmail.com>
2021-08-14 17:13:02 -0300

    Projeto criado

---
+++ src/main/java/com/grupotema/appportal/AppportalApplication.java
@@ -0,0 +1,13 @@
+package com.grupotema.appportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupotema/appportal/AppportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupotema.appportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
